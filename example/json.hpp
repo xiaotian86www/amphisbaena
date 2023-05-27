68722fbd824f50af98b22533f0bda37b307ebc01
duchang <xiaotian86www@163.com>
2023-05-27 13:14:10 +0800

    调整Object方法为虚方法

--- example/json.hpp
+++ example/json.hpp
@@ -2,35 +2,56 @@
 
 #include "translator.hpp"
 
-template <>
-class Object<rapidjson::Document>
+class JsonObject
+    : public translator::Object
 {
 public:
-    Object(rapidjson::Document &obj)
-        : obj_(obj) {}
+    JsonObject()
+        : obj_(rapidjson::kObjectType) {}
 
 public:
     int32_t get_value(
-        const char *name, int32_t default_value) const
+        std::string_view name, int32_t default_value) const override
     {
-        auto iter = obj_.FindMember(name);
+        auto iter = obj_.FindMember(rapidjson::StringRef(name.data(), name.size()));
         if (iter == obj_.MemberEnd())
             return default_value;
         else
             return iter->value.GetInt();
     }
 
-    void set_value(
-        const char *name, int32_t value)
+    std::string_view get_value(
+        std::string_view name, std::string_view default_value) const override
     {
-        auto iter = obj_.FindMember(name);
+        auto iter = obj_.FindMember(rapidjson::StringRef(name.data(), name.size()));
         if (iter == obj_.MemberEnd())
-            obj_.AddMember(rapidjson::Value(name, obj_.GetAllocator()),
+            return default_value;
+        else
+            return { iter->value.GetString(), iter->value.GetStringLength() };
+    }
+
+    void set_value(
+        std::string_view name, int32_t value) override
+    {
+        auto iter = obj_.FindMember(rapidjson::StringRef(name.data(), name.size()));
+        if (iter == obj_.MemberEnd())
+            obj_.AddMember(rapidjson::Value(name.data(), name.size(), obj_.GetAllocator()),
                            rapidjson::Value(value), obj_.GetAllocator());
         else
             iter->value.SetInt(value);
     }
 
+    void set_value(
+        std::string_view name, std::string_view value) override
+    {
+        auto iter = obj_.FindMember(rapidjson::StringRef(name.data(), name.size()));
+        if (iter == obj_.MemberEnd())
+            obj_.AddMember(rapidjson::Value(name.data(), name.size(), obj_.GetAllocator()),
+                           rapidjson::Value(value.data(), value.size()), obj_.GetAllocator());
+        else
+            iter->value.SetString(value.data(), value.size(), obj_.GetAllocator());
+    }
+
 private:
-    rapidjson::Document &obj_;
+    rapidjson::Document obj_;
 };
--- example/simple.cpp
+++ example/simple.cpp
@@ -2,12 +2,39 @@
 
 #include "json.hpp"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    rapidjson::Document obj(rapidjson::kObjectType);
-    Object<rapidjson::Document> obj_wrapper(obj);
+    translator::Context context;
 
-    obj_wrapper.set_value("field1", 1);
-    std::cout << "field1: " << obj_wrapper.get_value("field1", 0) << std::endl;
+    auto trans_field1 = translator::set_value(
+        "field1", translator::add(
+                      translator::lexical_cast<int32_t>(
+                          translator::get_value<std::string_view>("obj1", "field1", "0")),
+                      translator::lexical_cast<int32_t>(
+                          translator::get_value<std::string_view>("obj1", "field2", "0"))));
+    auto trans_field2 = translator::set_value(
+        "field2", translator::sub(
+                      translator::lexical_cast<int32_t>(
+                          translator::get_value<std::string_view>("obj1", "field1", "0")),
+                      translator::lexical_cast<int32_t>(
+                          translator::get_value<std::string_view>("obj1", "field2", "0"))));
+
+    context.set_object_func("obj1", [](translator::Context *context)
+                            {
+                                auto obj = std::make_unique<JsonObject>();
+                                obj->set_value("field1", "2");
+                                obj->set_value("field2", "1");
+                                return obj; });
+
+    context.set_object_func("obj2", [&trans_field1, &trans_field2](translator::Context *context)
+                            {
+                                auto obj = std::make_unique<JsonObject>();
+                                
+                                trans_field1(context, obj.get());
+                                trans_field2(context, obj.get());
+                                return obj; });
+
+    std::cout << "field1: " << context.get_object("obj2")->get_value("field1", 0) << ", "
+        << "field2: " << context.get_object("obj2")->get_value("field2", 0) << std::endl;
     return 0;
 }
