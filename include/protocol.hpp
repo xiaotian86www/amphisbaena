bf43cab25a56afb36ae00493db9936df56877ebe
duchang <xiaotian86www@163.com>
2023-07-08 07:34:21 +0800

    移动socket

--- include/protocol.hpp
+++ include/protocol.hpp
@@ -5,19 +5,9 @@
 #include <string_view>
 
 #include "schedule.hpp"
+#include "server.hpp"
 
 namespace translator {
-class Socket : public std::enable_shared_from_this<Socket>
-{
-public:
-  virtual ~Socket() = default;
-
-public:
-  virtual void send(ScheduleRef sch,
-                    CoroutineRef co,
-                    std::string_view data) = 0;
-};
-
 class Protocol
 {
 public:
