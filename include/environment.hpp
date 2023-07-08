8d95e8c13b3488804a27f5e3bc8dc5b7a1d0db7f
duchang <xiaotian86www@163.com>
2023-07-08 20:46:31 +0800

    扩展env

--- include/environment.hpp
+++ include/environment.hpp
@@ -11,12 +11,8 @@ namespace translator {
 class Environment
 {
 public:
-  Environment(ObjectFactoryPtr object_factory,
-              std::shared_ptr<ParserFactory> parser_factory,
-              std::shared_ptr<ProcessorFactory> processor_factory)
+  Environment(ObjectFactoryPtr object_factory)
     : object_pool_(object_factory)
-    , parser_factory_(parser_factory)
-    , processor_factory_(processor_factory)
   {
   }
 
@@ -27,16 +23,7 @@ public:
 
   Parser* get_server(std::string_view name);
 
-  std::unique_ptr<Parser> create_parser() { return parser_factory_->create(); }
-
-  std::unique_ptr<Processor> create_processor(std::string_view key)
-  {
-    return processor_factory_->create(key);
-  }
-
 private:
   ObjectPool object_pool_;
-  std::shared_ptr<ParserFactory> parser_factory_;
-  std::shared_ptr<ProcessorFactory> processor_factory_;
 };
 } // namespace translator
