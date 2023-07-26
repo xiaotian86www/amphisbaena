60b765343c1e7c787afbd21fdc0e91c80253b557
duchang <xiaotian86www@163.com>
2023-07-26 16:50:40 +0800

    新增plugin测试用例

---
+++ test/mock/builder/builder12.cpp
@@ -0,0 +1,25 @@
+
+#include <memory>
+
+#include "builder.hpp"
+#include "environment.hpp"
+#include "impl/json_message.hpp"
+#include "message.hpp"
+
+extern "C"
+{
+  void* get_func()
+  {
+    return new translator::MessageBuilder::ctor_function(
+      [](translator::Environment&, translator::MessagePtr) {
+        auto message = std::make_shared<translator::JsonMessage>();
+        message->get_body()->set_value("Field", 1);
+        return message;
+      });
+  }
+
+  const char* get_name2()
+  {
+    return "Message";
+  }
+}
---
+++ test/test_plugin.cpp
@@ -0,0 +1,43 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "builder.hpp"
+#include "environment.hpp"
+
+class Plugin : public testing::Test
+{
+public:
+  Plugin() {}
+  ~Plugin() { translator::MessageBuilder::unregiste(); }
+};
+
+TEST_F(Plugin, load)
+{
+  translator::Environment env;
+
+  translator::Plugin::load({ "mock/builder/libbuilder1.so" });
+  {
+    auto message = translator::MessageBuilder::create(
+      env, "Message", translator::MessagePtr());
+
+    EXPECT_EQ(message->get_body()->get_int("Field"), 1);
+  }
+
+  translator::Plugin::load({ "mock/builder/libbuilder13.so" });
+  {
+    auto message = translator::MessageBuilder::create(
+      env, "Message", translator::MessagePtr());
+
+    EXPECT_EQ(message->get_body()->get_int("Field"), 2);
+  }
+}
+
+TEST_F(Plugin, load_fail)
+{
+  translator::Environment env;
+
+  EXPECT_THROW(translator::Plugin::load({ "mock/builder/libbuilder11.so" }),
+               translator::CouldnotLoadException);
+  EXPECT_THROW(translator::Plugin::load({ "mock/builder/libbuilder12.so" }),
+               translator::CouldnotLoadException);
+}
