2c887a39abbdc7fc99e3e540fc69e9ee1a042b73
xiaotian86www <xiaotian86www@163.com>
2023-08-23 07:16:42 +0800

    新增文档

--- include/application.hpp
+++ include/application.hpp
@@ -1,7 +1,7 @@
 /**
  * @file application.hpp
  * @author duchang (xiaotian86www@163.com)
- * @brief 
+ * @brief 应用类
  * @version 0.1
  * @date 2023-08-22
  * 
@@ -24,8 +24,8 @@
 namespace amphisbaena {
 /**
  * @brief 应用类
- * 
- * 单例类，管理插件生命周期，提供load和reload方法，实现运行时插件加载和重加载。
+ *
+ * 单例类，管理插件生命周期。
  */
 class Application
 {
@@ -38,16 +38,28 @@ public:
 
 public:
   /**
-   * @brief 加载插件
+   * @brief 加载插件，先加载的插件在不主动卸载的情况下后释放，
+   * 这样保证了插件之间正确的依赖关系
    * 
-   * @param name 插件名，全局唯一
-   * @param path 插件库路径
-   * @param args 插件初始化参数
+   * @param[in] name 插件名，全局唯一
+   * @param[in] path 插件库路径
+   * @param[in] args 插件初始化参数
+   *
+   * @exception PluginNameExitsException 插件名已存在
    */
   void load(std::string_view name,
             const std::filesystem::path& path,
             const std::vector<std::string>& args);
 
+  /**
+   * @brief 重新加载插件，不会改变插件的加载顺序和释放顺序
+   * 
+   * @param[in] name 插件名，全局唯一
+   * @param[in] path 插件库路径
+   * @param[in] args 插件初始化参数
+   *
+   * @exception PluginNameNotExitsException 插件名不存在
+   */
   void reload(std::string_view name,
               const std::filesystem::path& path,
               const std::vector<std::string>& args);
--- include/plugin.hpp
+++ include/plugin.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file plugin.hpp
+ * @author duchang (xiaotian86www@163.com)
+ * @brief 插件类
+ * @version 0.1
+ * @date 2023-08-23
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #pragma once
 
 #include <filesystem>
@@ -5,16 +16,46 @@
 #include <vector>
 
 namespace amphisbaena {
+/**
+ * @brief 插件类，封装了插件的加载和卸载行为
+ *
+ * 如何定义一个插件？
+ * 
+ * - 需要将插件打包成一个动态库
+ * - 定义一个全局方法 @code {.c} void init(const char* const* args) @endcode，
+ * 并实现初始化逻辑，会在插件加载后调用
+ * - 定义一个全局方法（可选） @code {.c} void deinit() @endcode，
+ * 并实现析构逻辑，会在插件卸载前调用
+ */
 class Plugin
 {
 public:
+  /**
+   * @brief 构造函数，使用dlopen打开插件库，并调用插件的init方法
+   * 
+   * @param[in] name 插件名，全局唯一
+   * @param[in] path 插件库路径
+   * @param[in] args 插件初始化参数
+   *
+   * @exception CouldnotLoadException 不能打开插件，
+   * 当插件不存在或不存在init方法时，抛出该异常
+   */
   Plugin(std::string_view name,
          const std::filesystem::path& path,
          const std::vector<std::string>& args);
 
+  /**
+   * @brief 析构函数，调用插件的deinit方法（若存在的话），使用dlclose关闭插件库
+   * 
+   */
   ~Plugin();
 
 public:
+  /**
+   * @brief 返回插件名
+   * 
+   * @return std::string_view 
+   */
   std::string_view name() { return name_; }
 
 private:
