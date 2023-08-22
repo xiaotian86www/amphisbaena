3d9777821b232dd693c4d5bb40c5319147b7050a
xiaotian86www <xiaotian86www@163.com>
2023-08-22 22:45:54 +0800

    新增application注释

--- include/application.hpp
+++ include/application.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file application.hpp
+ * @author duchang (xiaotian86www@163.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #pragma once
 
 #include <filesystem>
@@ -11,6 +22,11 @@
 #include "plugin.hpp"
 
 namespace amphisbaena {
+/**
+ * @brief 应用类
+ * 
+ * 单例类，管理插件生命周期，提供load和reload方法，实现运行时插件加载和重加载。
+ */
 class Application
 {
 private:
@@ -21,6 +37,13 @@ public:
   static Application& get_instance();
 
 public:
+  /**
+   * @brief 加载插件
+   * 
+   * @param name 插件名，全局唯一
+   * @param path 插件库路径
+   * @param args 插件初始化参数
+   */
   void load(std::string_view name,
             const std::filesystem::path& path,
             const std::vector<std::string>& args);
